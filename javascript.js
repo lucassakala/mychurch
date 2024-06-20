260dc3a166d6703d9f5c11479f17b98b581a45b5
lucassakala <lukess403@gmail.com>
2024-06-20 17:05:27 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var info="hie";
+function message(){
+    var x= document.getElementById("myp").value;
+    if (x==0997415516){
+       
+    }
+    else{
+        alert("wrong number");
+    }
+}
+
